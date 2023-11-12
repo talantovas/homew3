9722c55996a158b47d634a61f089303b1cc8aa69
talantovas <stalantovva@gmail.com>
2023-11-13 01:22:04 +0600

    added

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LimitException {
 
         BankAccount account = new BankAccount(15000);
         System.out.println("Начальный баланс: " + account.getAmount() + " сом");
         account.deposit(5000);
-        try {
 
+        try {
             while (true) {
                 account.withdraw(6000);
-
             }
         } catch (LimitException e) {
             System.out.println("Ошибка: " + e.getMessage());
-            System.out.println("Оставшаяся сумма: " + e.getRemainingAmount() + " сом");
+            account.withdraw(e.getRemainingAmount());
         }
-
     }
 }
